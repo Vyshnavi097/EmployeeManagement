376736957b10e0309a504428af5cfb1985edd25c
Vyshnavi097 <vyshnavi.k@edstem.com>
2024-02-07 14:47:06 +0530

    test added

--- demo/src/test/java/com/example/demo/Controller/EmployeeControllerTest.java
+++ demo/src/test/java/com/example/demo/Controller/EmployeeControllerTest.java
@@ -8,43 +8,69 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static java.lang.reflect.Array.get;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 public class EmployeeControllerTest {
     private MockMvc mockMvc;
 
-        @InjectMocks
-        private EmployeeController employeeController;
+    @InjectMocks
+    private EmployeeController employeeController;
 
-        @Mock
-        private EmployeeService employeeService;
+    @Mock
+    private EmployeeService employeeService;
 
-        @Autowired
-        private WebApplicationContext webApplicationContext;
+    @Autowired
+    private WebApplicationContext webApplicationContext;
 
-        @BeforeEach
-        public void setUp() {
-            mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-        }
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
 
 
     @Test
     void testBooking() {
         EmployeeRequest Request = new EmployeeRequest("David", "da@gmail.com", "civil");
         EmployeeResponse Response =
-                new EmployeeResponse(1L,"David","da@gmail.com","civil");
+                new EmployeeResponse(1L, "David", "da@gmail.com", "civil");
         when(employeeService.create(Request)).thenReturn(Response);
     }
+
+    @Test
+    void testGetById() throws Exception {
+        when(employeeService.getById(anyLong()))
+                .thenReturn(new EmployeeResponse(1L, "Name", "name@example.org", "Department"));
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/employees/{id}", 1L);
+        MockMvcBuilders.standaloneSetup(employeeController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content()
+                        .string("{\"id\":1,\"name\":\"Name\",\"email\":\"name@example.org\",\"department\":\"Department\"}"));
+    }
+
+
+
+
 
 }
--- demo/src/test/java/com/example/demo/Service/EmployeeServiceTest.java
+++ demo/src/test/java/com/example/demo/Service/EmployeeServiceTest.java
@@ -13,11 +13,15 @@ import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -59,7 +63,26 @@ public class EmployeeServiceTest {
         assertEquals(expectedResponse, actualResponse);
     }
 
+    @Test
+    public void getByDepartTest() {
+        String dname = "Sales";
+        Employee employee1 = new Employee();
+        Employee employee2 = new Employee();
+        List<Employee> employees = Arrays.asList(employee1, employee2);
 
+        when(employeeRepository.findAll()).thenReturn(employees);
+
+        List<EmployeeResponse> result = employeeService.getByDepart(dname);
+
+        verify(employeeRepository).findAll();
+        assertEquals(2, result.size());
+
+        List<Employee> expectedEmployees = employees.stream()
+                .filter(employee -> employee.getDepartment().equals(dname))
+                .collect(Collectors.toList());
+
+        assertEquals(expectedEmployees.size(), result.size());
+    }
 
 
 }
